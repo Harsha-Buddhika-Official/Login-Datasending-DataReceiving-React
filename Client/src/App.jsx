5b968026a19f6e61d10e503193c826b403c27afd
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-07-22 18:01:11 +0530

    Route Set

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
-import Login from './Component/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './Dashboard'
+import LoginSignup from './LoginSignup'
 
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginSignup/>}/>
+          <Route path='/Dashboard' element={<Dashboard/>}/>
+          <Route path='/Login' element={<LoginSignup/>}/>
+          <Route path='/Signup' element={<LoginSignup/>}/>
+        </Routes>
+      
+      </BrowserRouter>
     </>
   )
 }
